668008f04be60712ce2a0e411b20ce6e83a689d1
aliva <ali.vakilzade@gmail.com>
2013-05-15 20:26:36 +0430

    admin page created

--- lamenews/admin.py
+++ lamenews/admin.py
@@ -1,3 +1,19 @@
+from lamenews import models
+
 from django.contrib import admin
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'creator',
+        'create_date',
+    )
+    filter_horizontal = (
+        'tags',
+    )
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Tag, TagAdmin)
