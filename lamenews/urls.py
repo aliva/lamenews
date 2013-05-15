be5d501e9474e4df76281059bd00d89120f11635
aliva <ali.vakilzade@gmail.com>
2013-05-15 22:48:51 +0430

    logout works

--- lamenews/urls.py
+++ lamenews/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    url(r'logout$', 'lamenews.views.logout', name='lamenews'),
+    url(r'login', 'lamenews.views.login', name='lamenews'),
     url(r'', 'lamenews.views.root', name='lamenews'),
 )
--- lamenews/views.py
+++ lamenews/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import logout as logout_django
+from django.core.urlresolvers import reverse
 
 def root(request):
+    return render(request, 'lamenews.html')
+
+def logout(request):
+    return logout_django(request, reverse('lamenews.views.root'))
+
+def login(request):
     return render(request, 'lamenews.html')
