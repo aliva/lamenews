bbeba404d5186b354e7d906ef5d3913961bbd74e
aliva <ali.vakilzade@gmail.com>
2013-05-15 20:26:21 +0430

    __str__ added to modes

--- lamenews/models.py
+++ lamenews/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class Post(models.Model):
-    post = models.TextField()
     title = models.CharField(max_length=140,
                              unique=True) 
+    post = models.TextField()
     tags = models.ManyToManyField('Tag')
     creator = models.ForeignKey(get_user_model())
-    create_date = models.DateTimeField(auto_now_add=True) 
+    create_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title 
     
 class Tag(models.Model):
     name = models.CharField(max_length=140)
+    
+    def __str__(self):
+        return self.name
