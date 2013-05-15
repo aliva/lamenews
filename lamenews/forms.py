89312e716f70b98ca1e5bffc1f6e475c950066a2
aliva <ali.vakilzade@gmail.com>
2013-05-16 00:40:34 +0430

    user can register

---
+++ lamenews/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    error_messages = UserCreationForm.error_messages
+    error_messages['duplicate_email'] = "a user with that email already exists"
+    
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "email")
+        
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        try:
+            get_user_model()._default_manager.get(email=email)
+        except get_user_model().DoesNotExist:
+            return email
+        raise forms.ValidationError(self.error_messages['duplicate_email'])
--- lamenews/views.py
+++ lamenews/views.py
@@ -1,16 +1,32 @@
+from lamenews import forms
+
 from django.shortcuts import render
-from django.contrib.auth.views import logout as logout_django
-from django.contrib.auth.views import login as login_django
+from django.contrib.auth.views import logout as logout_view
+from django.contrib.auth.views import login as login_view
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_user
 
 def root(request):
     return render(request, 'lamenews.html')
 
 def logout(request):
-    return logout_django(request, reverse('lamenews.views.root'))
+    return logout_view(request, reverse('lamenews.views.root'))
 
 def login(request):
-    return login_django(request, 'lamelogin.html')
+    return login_view(request, 'lamelogin.html')
 
 def register(request):
-    return login_django(request, 'lamelogin.html')
+    if request.method == "POST":
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid() == True:
+            form.save()
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            login_user(request, user)
+            return root(request)
+    else:
+        form = forms.RegisterForm()
+    context = {
+        'register_form': form,
+    }
+    return render(request, 'lameregister.html', context)
