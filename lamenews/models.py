0a82cd8a60ed807b920581a4976854d0375e2adb
aliva <ali.vakilzade@gmail.com>
2013-05-15 20:10:18 +0430

    basic model created

--- lamenews/models.py
+++ lamenews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Post(models.Model):
+    post = models.TextField()
+    title = models.CharField(max_length=140,
+                             unique=True) 
+    tags = models.ManyToManyField('Tag')
+    creator = models.ForeignKey(get_user_model())
+    create_date = models.DateTimeField(auto_now_add=True) 
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=140)
